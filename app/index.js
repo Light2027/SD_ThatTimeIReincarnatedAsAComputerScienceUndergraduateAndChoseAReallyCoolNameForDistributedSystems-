e7a70d7f91c2843369fc36d72c0df22ffd5e1d68
Galfi Istvan Balazs <112966@fhwn.ac.at>
2021-12-17 09:03:32 +0100

    Changed hello World to Hello Saurer.

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (_, res) {
-  res.send('Hello World!')
+  res.send('Hello Saurer!')
 })
  
 const port = process.env.PORT || 8080;
